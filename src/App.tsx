21336487587590854f7e08210a1b2c535d6f1cf5
unknown <fernandoalvesq@gmail.com>
2021-07-14 18:08:36 -0300

    2-4-1 Instalando a biblioteca modal e utilizando ela na nossa aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
+import { useState } from "react";
 import { DeashBoard } from "./components/DashBoard";
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransaction] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransaction(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransaction(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <DeashBoard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h1>Cadastrar transação</h1>
+      </Modal>
+
       <GlobalStyle />
    </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import imgLogo from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-export function Header () {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void ;
+}
+
+export function Header ( { onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content >
         <img src={imgLogo} alt="Dt money" />
-        <button type="button" >
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
         </button>
       </Content>
